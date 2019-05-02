6d8baa695dcd44bd30f432a267cd6ad782e82bed
Sam Schmidt <sschmidt@physics.ucdavis.edu>
2019-05-01 18:42:27 -0700

    switch to hdf5 writing, eliminate non-functioning output specification

--- bpzpipe/bpzpipeStage1.py
+++ bpzpipe/bpzpipeStage1.py
@@ -16,9 +16,13 @@ class BPZpipeStage1(PipelineStage):
     param_file: ascii file containing the BPZ parameters other than INTERP, infile, and outfiles.  
       Many of the parameters will  be the same run to run, so we can use a default file for many 
       healpix pixels' of data.
-    INTERP: integer (though read in as a strint): the number of interpolated SEDs to be created
+    INTERP: integer (though read in as a string): 
+      the number of interpolated SEDs to be created
       in between the grid.  The default value is 0.
       path_to_bpz: string
+    OUTPUT_CUT: float (read in as string)
+      If OUTPUT_CUT is set, then BPZ masks values with M_0 (the reference band used in the prior)
+      and only computes and outputs photo-z's for galaxies with M_0 < OUTPUT_CUT
     path_to_BPZ: string
       The path to the installed BPZpy3 version, will be set as the $BPZPY3PATH env. variable
     outputs:
@@ -37,23 +41,23 @@ class BPZpipeStage1(PipelineStage):
         ('input_photometry_file', HDFFile),
     ]
     outputs = [
-        ('photoz_pointfile', TextFile),
-        ('photoz_probsfile', TextFile),
-        # More inputs can go here
+        ('output_photoz_pointfile', HDFFile),
+        ('output_photoz_probsfile', HDFFile)
     ]
     config_options = {
         'INTERP': '0',  #Interpolate between input templates N times
+        'OUTPUT_CUT': "Null", #if you want to only compute and output M_0 < OUTPUT_CUT set here
         'param_file': "test.pars",
         'path_to_bpz': "/global/projecta/projectdirs/lsst/groups/PZ/BPZ/BPZpy3/pz_pdf/pz/BPZ"
         }
 
     def run(self):
-        outfile_point = self.get_output('photoz_pointfile')
-        outfile_probs= self.get_output('photoz_probsfile')
         interp = self.config['INTERP']
         pfile = self.config['param_file']
         infile = self.get_input('input_photometry_file')
         bpz_path = self.config['path_to_bpz']
+        outfile_point = self.get_output('output_photoz_pointfile')
+        outfile_probs= self.get_output('output_photoz_probsfile')
         #BPZ uses paths relevant to an environment variable, set this
         os.environ["BPZPY3PATH"]=bpz_path
         #BPZ is so old that it has some leftover references to NUMERIX
@@ -61,8 +65,10 @@ class BPZpipeStage1(PipelineStage):
         print ("Running BPZ...")
         #Set up the command line command to run bpz_py3_hdf5.  The format is
         #bpz_py3_hdf5.py [infile] -P [parsfile] [specific BPZ keywords not in pars file, e.g. -INTERP]
+        bpz_exec = bpz_path+"/bpz_py3_readwritehdf5.py"
+
         args = ['python3',
-                '/global/projecta/projectdirs/lsst/groups/PZ/BPZ/BPZpy3/pz_pdf/pz/BPZ/bpz_py3_hdf5.py',
+                bpz_exec,
                 infile,
                 '-P',pfile,'-OUTPUT',
                 outfile_point,'-PROBS_LITE',outfile_probs]
