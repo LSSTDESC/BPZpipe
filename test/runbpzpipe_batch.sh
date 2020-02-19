651198c8b85f409ef517bbe744dfda0ac0682619
Sam Schmidt <sschmidt@physics.ucdavis.edu>
2020-02-19 15:08:16 -0800

    add parsl path and delete custom conda env from script

--- test/runbpzpipe_batch.sh
+++ test/runbpzpipe_batch.sh
@@ -7,7 +7,6 @@
 #SBATCH --output="bpzpipe_test1000.out"
 
 module load python/3.6-anaconda-4.4
-source activate /global/homes/s/schmidt9/CONDA/BPZPIPE/conda-bpz-new
 module swap PrgEnv-intel PrgEnv-gnu
 module load PrgEnv-gnu
 module unload darshan
@@ -17,6 +16,6 @@ module load gsl/2.5
 
 export CECI_SETUP="/global/projecta/projectdirs/lsst/groups/PZ/BPZ/BPZpipe/test/setup-cori-update"
 export HDF5_USE_FILE_LOCKING=FALSE
-export PYTHONPATH=$PYTHONPATH:/global/projecta/projectdirs/lsst/groups/PZ/BPZ/BPZpipe:/global/homes/s/schmidt9/DESC/software/descformats/DESCFormats/descformats:/global/homes/s/schmidt9/DESC/software/ceci/ceci
+export PYTHONPATH=$PYTHONPATH:/global/projecta/projectdirs/lsst/groups/PZ/BPZ/BPZpipe:/global/homes/s/schmidt9/DESC/software/descformats/DESCFormats/descformats:/global/homes/s/schmidt9/DESC/software/ceci/ceci:/global/projecta/projectdirs/lsst/groups/PZ/Packages/parsl0.5.2/lib/python3.7/site-packages
 
 srun -n 6 python3 -m bpzpipe BPZ_pz_pdf  --photometry_catalog=./test1000_h5pyfmt_new.h5 --config=./config.yml --photoz_pdfs=./outputs/test1000_outputfile.hdf5 --mpi
